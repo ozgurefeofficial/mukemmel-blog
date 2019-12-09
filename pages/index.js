6d6b860792461077dba0edccf43e3356cdee80dc
Özgür Efe <52582156+ozgurefeofficial@users.noreply.github.com>
2019-12-09 23:30:28 +0300

    index.js ve [postId].js güncelleme
    navbar eklendi tema renk ve fontu değiştirildi. postlar arası geçişin fark edilirliği artması için <hr> eklendi.

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,22 @@ import Head from "next/head";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 
-// BOOTSTRAP EKLENDI - Nav bar tasarımı yapılıyor
+// BOOTSTRAP EKLENDI - Nav bar tasarımı
 const Home = ({ posts }) => (
   <div className="container">
+
     <Head>
       <title>Ana Sayfa</title>
       <link rel="icon" href="/favicon.ico" />
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,700,900&display=swap" rel="stylesheet"></link>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <nav class="navbar navbar-full navbar-light bg-light sticky-top eztt" >
+      <a class="navbar-brand" href="#"><font color="#16A662" face="Poppins"><b>ozgurefe.com</b></font></a>
+
+</nav>
     </Head> 
 
     <div className="hero">
@@ -44,18 +50,28 @@ const Home = ({ posts }) => (
         <div className="blog-text">
           <ReactMarkdown source={post.details} />
         </div>
-        <div className="blog-date">{post.date}</div>
+        <div className="blog-date">{post.date} <hr></hr></div>
       </div>
     ))}
 
     <style jsx>{`
+      .blog-title {
+        font-family: Poppins;
+        font-weight: bold;
+        color: #0D663D;
+      }
+      .navbar {
+        font-family: Poppins;
+      }
+      .blog-text {
+        font-family: Poppins;
+      }
       .container {
         max-width: 650px;
         width: 100%;
         margin: 0 auto;
-        backgroun
       }
-
+      
       .hero {
         text-align: center;
         margin: 96px 0;
@@ -64,19 +80,27 @@ const Home = ({ posts }) => (
       .social-link {
         margin-right: 8px;
       }
-
+      .eztt {
+        font-family: Poppins;
+        font-weight: bold;
+        color: #16A662;
+      }
       .hero-title {
         font-size: 48px;
+        font-family: Poppins;
+        font-weight: bold;
+        color: #16A662;
       }
 
       .blog-date {
         text-align: right;
         color: #cccccc;
         margin: 12px 0 48px 0;
+        font-family: Poppins;
       }
 
       a {
-        color: #35459e;
+        color: #1EE688;
         text-decoration: none;
       }
     `}</style>
